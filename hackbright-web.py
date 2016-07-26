0ed7dbf5adeb9b515076917c6473e24ae9d9834a
Hackbright Student <info@hackbrightacademy.com>
2016-07-26 15:48:53 -0700

    Completed step 1: added new routes to display and handle form.

--- hackbright-web.py
+++ hackbright-web.py
@@ -9,9 +9,23 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
-    first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    github = request.args.get('github','jhacks')
+    try:
+        first, last, github = hackbright.get_student_by_github(github)
+    except:
+        return "You need to enter a github user name."
+
+    return render_template("student_info.html",
+                            first=first,
+                            last=last,
+                            github=github)
+
+@app.route("/search")
+def get_student_form():
+    """Display search form."""
+
+    return render_template("student_search.html")
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
--- hackbright.py
+++ hackbright.py
@@ -29,8 +29,7 @@ def get_student_by_github(github):
         """
     db_cursor = db.session.execute(QUERY, {'github': github})
     row = db_cursor.fetchone()
-    print "Student: %s %s\nGithub account: %s" % (row[0], row[1],
-                                                  row[2])
+    # print "Student: %s %s\nGithub account: %s" % (row[0], row[1], row[2])
     return row
 
 
